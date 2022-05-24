65e0841541398c6618e3166033b4c6a8c79bfc49
Igor Berlenko <sevenflash@yandex.ru>
2022-05-25 07:17:52 +0800

    Third - May 25th

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,17 @@ import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react';
 
 import { supabase } from './supabaseClient';
 
-function ViewProfile({ address }: any) {
+import slugo from 'slugo';
+
+function ViewProfile({ address, slug }: any) {
   const [name, setName] = React.useState('');
+  const [profileAddress, setProfileAddress] = React.useState('');
 
   useEffect(() => {
-    supabase.from('profiles')
+    if (address) {
+      setProfileAddress(address);
+
+      supabase.from('profiles')
       .select('name')
       .eq('address', address)
       .single()
@@ -19,6 +25,21 @@ function ViewProfile({ address }: any) {
           setName('deleted');
         }
       });
+    } else if (slug) {
+      supabase.from('profiles')
+      .select('name, address')
+      .eq('slug', slug)
+      .single()
+      .then(result => {
+        if (result.data) {
+          setName(result.data.name);
+          setProfileAddress(result.data.address);
+        } else {
+          setName('deleted');
+          setProfileAddress('');
+        }
+      });      
+    }
   }, []);
 
   return (
@@ -31,7 +52,7 @@ function ViewProfile({ address }: any) {
             }
           </div>
           <div className="px-8 my-2 flex flex-row justify-start">
-            <h1 className="bg-gray-600 p-2">{address}</h1>
+            <h1 className="bg-gray-600 p-2">{profileAddress}</h1>
               <h1 className="p-2">Address</h1>
           </div>
           <div className="px-8 my-2 flex flex-row justify-start">
@@ -57,17 +78,26 @@ function MyProfile() {
 
   const [location, setLocation] = useLocation();
   const [name, setName] = React.useState('anonymous');
+  const [slug, setSlug] = React.useState('');
   const nameRef: any = React.createRef();
 
   useEffect(() => {
+    // if(!slug && address) {
+    //   setSlug(address);
+    // }
+
     supabase.from('profiles')
-      .select('name')
+      .select('name, slug')
       .eq('address', address)
       .single()
       .then(result => {
         if (result.data) {
-          console.log('supabase name', result.data.name);
+          console.log('supabase slug', result.data.slug);
           setName(result.data.name);
+
+          if (result.data.slug) {
+            setSlug(result.data.slug);
+          }
         } else {
           console.error('cannot find profile', result.error)
         }
@@ -85,10 +115,15 @@ function MyProfile() {
       if (location.split('/')[2]) {
         const newName = nameRef.current.value;
         setName(newName);
+
         // window.localStorage.setItem('name', newName);
+
+        const slug = `${slugo(newName)}-${address?.substring(39)}`;
+
         supabase.from('profiles').upsert({
           address: address,
           name: newName,
+          slug: slug
         }, {
           onConflict: 'address'
         }).then(console.log);
@@ -107,7 +142,10 @@ function MyProfile() {
           <div className="px-8 my-4 flex flex-row justify-start">
             <h1 className="">My Profile</h1>
             <span className="px-2">|</span>
-            <a target="_blank" href={`/${address}`} className="underline">Share Link</a>
+            {
+              slug ? <a target="_blank" href={`/profile/${slug}`} className="underline">Share Link</a> :
+              <a target="_blank" href={`/${address}`} className="underline">Share Link</a>
+            }
           </div>
           <div className="px-8 my-2 flex flex-row justify-start">
             <h1 className="bg-gray-600 p-2">{address}</h1>
@@ -147,13 +185,13 @@ function MyProfile() {
 function App() {
   const [location, setLocation] = useLocation();
 
-  return (
-    location.startsWith('/0x') ? (
-      <ViewProfile address={location.split('/')[1]} />
-    ) : (
-      <MyProfile />
-    )
-  );
+  if (location.startsWith('/0x')) {
+    return <ViewProfile address={location.split('/')[1]} />;
+  } else if (location.startsWith('/profile')) {
+    return <ViewProfile slug={location.split('/')[2]} />;
+  } else {
+    return <MyProfile />;
+  }
 }
 
 export default App;
